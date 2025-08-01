79f5c9030265c6e731c797ba501de5a24e85b8ef
tinyakov_daniil <tinyakov-d@mail.ru>
2025-08-01 14:37:35 +0300

    Test and update Views

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import Post
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='testUser')
+        self.post = Post.objects.create(
+            author=self.user,
+            title='Test Title',
+            content='Test Content'
+        )
+        
+
+    def test_post_creation(self):
+        self.assertEqual(self.post.title,'Test Title')
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    
